dae53069b6612775bb17a37c8cd06ce699f48f79
Dan Lee <danielhlee4@gmail.com>
2023-07-21 15:26:07 -0400

    add #two_sum

--- ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -1,11 +1,30 @@
 require_relative "../lib/tdd.rb"
 
-describe "#my_uniq"
+describe "#my_uniq" do
     it "removes duplicate elements from the array" do
-        expect([1,2,1,3,3].my_uniq).to eq([1,2,3])
+        expect(my_uniq([1,2,1,3,3])).to eq([1,2,3])
     end
 
     it "returns the unique elements in the order in which they appear" do
-        expect([3,1,1,2].my_uniq).to eq([3,1,2])
+        expect(my_uniq([3,1,1,2])).to eq([3,1,2])
+    end
+
+    context "when an empty array is passed in" do
+        it "returns an empty array" do
+            expect(my_uniq([])).to eq([])
+        end
+    end
+end
+
+
+#case: returns empty array if no pairs sum to zero
+#case: returns all pairs of indices that sum to zero
+#case: returns pairs in "dictionary-wise" order
+# does not return a pair of the same indices ex:[0,0]
+describe Array do
+    describe "#two_sum" do
+        it "returns empty array if no pairs sum to zero" do
+            expect([-1, 0, 7, 2, -4].two_sum).to eq([])
+        end
     end
 end
